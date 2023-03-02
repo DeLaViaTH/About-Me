b05b050fffdbe54c3eb99060d48cb69436d6fdea
DeLaViaTH <natchanon0879771509@gmail.com>
2023-03-02 20:04:33 +0700

    update
    add page

---
+++ page/navbar.js
@@ -0,0 +1,23 @@
+const navbar = `
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" >DeLaViaTH</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="bmi.html">BMI Calculator</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://status-page.delaviath.com/">Status Page</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+`;
